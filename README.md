c21187174b23289fd9829f2f9494d0bbfd857ffe
osrsbox <phoil@osrsbox.com>
2018-08-04 08:21:38 +1200

    Tidied repository. Added new compiled version of RuneLite cache tool. Updated README.md to point to new website.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # osrs-cache
-## OSRS Cache Documentation
 
-For more information see: http://osrsbox.com/osrsbox-cache
+This repository hosts a collection of tools and scripts for investigating the Old School RuneScape Cache - primarily using the RuneLite client and associated suite of tools.
+
+For more information see: https://www.osrsbox.com/blog/tags/Cache/
---
+++ determine_cache_version/osrs_cache_version.sh
@@ -0,0 +1,52 @@
+DEOB_PATH="deob-1.2.9-SNAPSHOT-jar-with-dependencies.jar"
+JAV_CONFIG="jav_config.ws"
+VANILLA="vanilla.jar"
+
+rm $JAV_CONFIG
+rm $VANILLA
+
+echo ">>> Starting..."
+echo "  > Downloading: $JAV_CONFIG"
+wget -q oldschool.runescape.com/jav_config.ws
+
+# Check the file was downloaded sucessfully
+if [[ "$?" != 0 ]]; then
+    echo ">>> Error downloading file. Exiting."
+    exit 1
+else
+    echo "  > Download sucessful."
+fi
+
+# Get the base URL for download
+BASEURL=$(grep codebase $JAV_CONFIG | cut -d'=' -f2)
+echo "  > Base URL used: $BASEURL"
+
+# Get the JAR file name for download
+INITIAL_JAR=$(grep initial_jar $JAV_CONFIG | cut -d'=' -f2)
+echo "  > Initial JAR file for download: $INITIAL_JAR"
+
+# Combine base URL and JAR file name
+JAR_URL=$BASEURL$INITIAL_JAR
+
+# Download the JAR file
+echo "  > Downloading: $JAR_URL"
+wget -q $JAR_URL -O $VANILLA
+
+# Check the file was downloaded sucessfully
+if [[ "$?" != 0 ]]; then
+    echo ">>> Error downloading file. Exiting."
+    exit 1
+else
+    echo "  > Download sucessful."
+fi
+
+# Extract the cache version
+VANILLA_VER=$(java -cp $DEOB_PATH net.runelite.deob.clientver.ClientVersionMain $VANILLA)
+echo "  > OSRS version: $VANILLA_VER"
+
+# Clean up files
+if [ -f "$file" ]; 
+rm $JAV_CONFIG
+rm $VANILLA
+
+echo ">>> Finished."
